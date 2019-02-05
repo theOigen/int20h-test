73ba7f0f7d201b88afabff630ff92a04c345b5ed
T_GulDan <grayman219@gmail.com>
2019-02-05 17:54:40 +0200

    Landing of the HomePage

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -63,9 +63,9 @@
 </template>
 
 <script>
-export default {
-  name: 'HomePage',
-  data () {
+  export default {
+    name: 'HomePage',
+    data() {
       return {
         photos: null,
         page: 0,
@@ -94,23 +94,26 @@ export default {
         }
       }
     },
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
