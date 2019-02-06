8db22dbbc16616693e4abcbd2493a4b6fd151ecf
mihamor <mishamoroz5@gmail.com>
2019-02-06 03:13:43 +0200

    commit for merge

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 Vue.use(Vuex);
 
-const NUMBER_OF_PHOTOS_PER_PAGE = 12;
+const NUMBER_OF_PHOTOS_PER_PAGE = 6;
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
@@ -19,12 +19,12 @@ export const store = new Vuex.Store({
         // not implemented yet
     },
     actions: {
-      async getPhotos(context, credentials) {
-        const response = await axios.get('/api/v1/photos?page=' + credentials.page + '&per_page=' + NUMBER_OF_PHOTOS_PER_PAGE + '&raw=true');
-        console.log('response from server:', response);
-        if (response.data.error)
-          throw new Error(response.data.error);
-        return response.data.photos;
-      },
+        async getPhotos(context, credentials) {
+            const response = await axios.get('/api/v1/photos?page=' + credentials.page + '&per_page=' + NUMBER_OF_PHOTOS_PER_PAGE + '&raw=true');
+            console.log('response from server:', response);
+            if (response.data.error)
+                throw new Error(response.data.error);
+            return response.data.photos;
+        },
     }
 });
